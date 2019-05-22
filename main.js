e0bf3a2ea970571b90898fe166c743cfea779aaf
haozhuang <1340335269@qq.com>
2019-05-22 21:11:35 +0800

    点击关闭浮层

---
+++ main.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+    clickMe.addEventListener('click', function(e){
+        popover.style.display = 'block'
+    })
+    wrapper.addEventListener('click', function(e){
+        e.stopPropagation()
+    })
+      
+    document.addEventListener('click', function(){
+        popover.style.display = 'none'
+    })
+}
